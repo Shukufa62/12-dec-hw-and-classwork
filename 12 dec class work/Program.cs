42aa2ed4f5b5839776b53d729093f803b38bbaf1
Shukufa62 <119412605+Shukufa62@users.noreply.github.com>
2022-12-13 00:12:59 +0400

    Add files via upload

---
+++ 12 dec class work/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace _12_dec_class_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            int number = 90;
+            var result = Increment(number);
+            Console.WriteLine(result);
+
+
+
+            string fulname = Makefullname("aysel", "cavadzade");
+            Console.WriteLine(fulname);
+
+            var sumresult = Sum(90, 91);
+
+            Console.WriteLine(sumresult);
+
+
+            string num1Str = Console.ReadLine();
+            int n1 = Convert.ToInt32(num1Str);
+            string num2str = Console.ReadLine();
+            int n2 = Convert.ToInt32(num2str);
+            var result1 = sum(n1, n2);
+                Console.WriteLine( result1 );
+
+
+
+
+        }
+
+
+        static int Increment(int num)
+        {
+            num++;
+            return num;
+        }
+
+        static string Makefullname(string name,string surname)
+
+        {
+
+            var fn = $"{name} {surname}";
+            return fn;
+
+            
+
+
+        }
+
+
+        static int Sum(int num1,int num2)
+        {
+            return num1 + num2;
+        }
+
+
+
+        static int sum(int n1,int n2)
+        {
+            return n1 + n2;
+        }
+    }
+
+
+}
+
+        
---
+++ 12dec hw/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace _12dec_hw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var result = Sum(135, 64);
+            Console.WriteLine(result);
+
+
+
+            var answer = square(7);
+            Console.WriteLine(answer);
+
+
+
+            var check = letter("kazim");
+            Console.WriteLine(check);
+
+
+            var average1 = average(3, 18, 44, 75);
+            Console.WriteLine(average1);
+
+
+            var answer2 = sum1(275);
+            Console.WriteLine(answer2);
+
+
+        }
+
+     //Verilmiş iki ədədi toplayıb nəticəsini qaytaran metod
+        static int Sum(int n1,int n2)
+        {
+            return n1 + n2;
+        }
+
+        //Verilmiş ədədin kvadratını qaytaran metod
+
+        static int square(int num1)
+        {
+            return num1 * num1;
+        }
+
+
+
+        //Verilmiş string dəyərində a hərfinin olub olmadığını qaytaran metod
+
+
+        static bool letter (string word)
+        {
+
+            var i = 0;
+                return (word[i] == 'a') || word[i] == 'A';
+            
+            
+        }
+
+        //Verilmiş 4 ədədin ortalamasını tapıq qaytaran metod
+
+
+
+
+        static int average(int a,int b,int c,int d)
+        {
+            return (a + b + c + d) / 4;
+        }
+         //Verilmiş ədədin rəqəmləri cəmini qaytaran metod
+         static int sum1(int number)
+        {
+            int numbers = (number % 10) + (number - number % 10) / 10;
+            if (number < 0)
+                return numbers;
+
+ 
+           
+            
+            
+
+                
+
+            
+           
+            
+
+
+
+        }
+    }
+}
+
